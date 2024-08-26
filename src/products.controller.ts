027ea57203b7c33a21940872d72b2297548849e2
Thiago Vasconcelos <thiagovt.dev@gmail.com>
2024-08-26 09:17:05 -0300

    feat: Add Product model and update ProductsController

--- src/products.controller.ts
+++ src/products.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {
   Body,
   Controller,
@@ -7,39 +8,40 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { Product } from './product.model';
 
 @Controller('products')
 export class ProductsController {
+  products: Product[] = [
+    new Product('liv001', 'livro 1', 39.99),
+    new Product('liv002', 'livro 2', 29.99),
+    new Product('liv003', 'livro 3', 19.99),
+  ];
   @Get()
-  public getAllProducts(): string {
-    return 'List all products';
+  public getAllProducts(): Product[] {
+    return this.products;
   }
 
   @Get(':id')
-  public getProduct(@Param() params): string {
-    return 'Get a product with id: ' + params.id;
+  public getProduct(@Param() params): Product {
+    return this.products[0];
   }
 
   @Post('add-product')
-  public createProduct(@Body() product): string {
+  public createProduct(@Body() product: Product) {
     console.log(product);
-    return 'Create a product: ' + JSON.stringify(product);
+    product.id = this.products.length + 1;
+    return this.products.push(product);
   }
 
   @Put('update-product/:id')
-  public updateProduct(@Param() params, @Body() product): string {
-    console.log(params.id);
-    console.log(product);
-    return (
-      'Update a product with id: ' +
-      params.id +
-      ' with data: ' +
-      JSON.stringify(product)
-    );
+  public updateProduct(@Param() params, @Body() product: Product): Product {
+    return product;
   }
 
   @Delete('delete/:id')
   public deleteProduct(@Param() params): string {
-    return 'Delete a product with id: ' + params.id;
+    this.products.pop();
+    return 'Product deleted';
   }
 }
