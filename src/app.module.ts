1a539ba224bcc454cc77847f74a316ba8ee2a1ac
Thiago Vasconcelos <thiagovt.dev@gmail.com>
2024-08-27 08:01:52 -0300

    chore: Add @nestjs/config dependency and update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products.controller';
+import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 
 @Module({
-  imports: [],
+  imports: [EnvConfigModule],
   controllers: [AppController, ProductsController],
   providers: [AppService],
 })
